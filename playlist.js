afb866cfc6d7bcdeb49c26310c5740c54d84b77b
ching0423 <candy16257@gmail.com>
2022-04-12 00:00:16 +0800

    first commit

---
+++ playlist.js
@@ -0,0 +1,14 @@
+//YouTube Video ID
+let playList = [
+    "XfTWgMgknpY",
+    "kqqxZ_FXgVE",
+    "ESx_hy1n7HA",
+ 
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [150,215],
+    [024,057],
+    [000,020],
+   
+    ];
